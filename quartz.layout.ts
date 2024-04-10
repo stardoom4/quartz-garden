01aa0e857d5c46d57ed3c4201dcad2ad3ce735d0
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-04-10 13:43:38 +0530

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,9 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/stardoom4/quartz-garden",
+      Bookmarks: "https://raindrop.io/Celestial-Entity/main-43067410",
+      "Revered-Mother": "https://garud.netlify.app/Bharat-Mata",
     },
   }),
 }
