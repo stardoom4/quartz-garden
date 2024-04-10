26defad7f5dbc60bf8c1d4e82526af271eebac48
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-04-10 13:45:10 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -38,7 +38,7 @@ const config: QuartzConfig = {
           highlight: "rgba(143, 159, 169, 0.15)",
         },
         darkMode: {
-          light: "#1000000",
+          light: "#000000",
           lightgray: "#6D0D19",
           gray: "#646464",
           darkgray: "#d4d4d4",
