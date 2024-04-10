ce548af3791afe2f361f5a197de41ae24dba4085
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-04-10 13:51:45 +0530

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -48,3 +48,18 @@ export const defaultListPageLayout: PageLayout = {
   ],
   right: [],
 }
+
+Component.Explorer({
+  title: "Explorer", // title of the explorer component
+  folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
+  folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
+  useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
+  // Sort order: folders first, then files. Sort folders and files alphabetically
+  sortFn: (a, b) => {
+    ... // default implementation shown later
+  },
+  filterFn: filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
+  mapFn: undefined,
+  // what order to apply functions in
+  order: ["filter", "map", "sort"],
+})
